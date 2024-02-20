82f9e6f7152d11f119515d190fa4b5fa6494528a
adde9708 <adde.edvin@gmail.com>
2024-02-20 19:03:06 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace nodups
 {
@@ -25,39 +26,60 @@ namespace nodups
             bool allWordsDistinct = words.Length == words.Distinct().Count();
             bool allCharactersUpperCase = words.All(word => word.All(char.IsUpper));
             
+
             return allWordsDistinct && allCharactersUpperCase ? yes : no;
         }
-
+        
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void PrintResult(string phrase)
         {
             Console.WriteLine(NoDup(phrase));
         }
-
-        static void Main()
-        {   
-            // This works in Visual Studio but not Kattis
-            /*for (int i = 0; i < 3; i++)
+        
+        private static void LoopSolution()
+        {
+            for (int i = 0; i < 3; i++)
             {
                 Console.Write("Enter a phrase:");
                 string? phrase = Console.ReadLine();
-                if (phrase != null) PrintResult(phrase);
+                if (phrase != null)  PrintResult(phrase);
             }
-            */
-            /* Also works in visual studio but not Kattis
+        }
+        private static void RecursiveSolution()
+        {
             const string phrase0 = "THE RAIN IN SPAIN";
             const string phrase1 = "IN THE RAIN AND THE SNOW";
             const string phrase2 = "THE RAIN IN SPAIN IN THE PLAIN";
             PrintResult(phrase0);
             PrintResult(phrase1);
             PrintResult(phrase2);
-            */
-           
-            // This works in Kattis for one test case
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void CallOnceSolution()
+        {
             const string phrase0 = "THE RAIN IN SPAIN";
             PrintResult(phrase0);
         }
+        
+        static void Main()
+        {
+            // This works in Visual Studio but not Kattis
+            LoopSolution();
+            Console.WriteLine();
+
+            // Also works in visual studio but not Kattis
+            RecursiveSolution();
+            
+            Console.WriteLine();
+            
+            // This works in Kattis for one test case
+            CallOnceSolution();
+            
+        }
+
+      
     }
-
-
-
+    
 }
